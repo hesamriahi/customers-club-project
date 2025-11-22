66f5d9b3d7a053b1d7fa357097b26ebb96691de0
Hesam <h.riahi017@gmail.com>
2025-11-22 14:56:19 +0330

    first commit

---
+++ packages/hesamriahi/customers-club/src/CustomersClubServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hesamriahi\CustomersClub;
+
+use Illuminate\Support\ServiceProvider;
+
+class CustomersClubServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
